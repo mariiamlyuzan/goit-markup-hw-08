0527c7733e83ec85ed3fdafbbfb63092445e25e4
mariiamlyuzan <86401047+mariiamlyuzan@users.noreply.github.com>
2021-08-26 00:18:40 +0300

    Добавляет медиа-правила

--- js/modal.js
+++ js/modal.js
@@ -9,6 +9,7 @@
     refs.closeModalBtn.addEventListener('click', toggleModal);
   
     function toggleModal() {
+      document.body.classList.toggle("modal-open");
       refs.modal.classList.toggle('is-hidden');
     }
   })();
@@ -27,3 +28,5 @@
       mobileMenuRef.classList.toggle("is-open");
     });
   })();
+
+  
